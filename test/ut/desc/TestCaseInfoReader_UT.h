9bc47f27baf7fa5de4eb23a74b04d7758d3226ed
Arthur Yuan <arthur.ii.yuan@gmail.com>
2010-02-22 00:13:38 +0800

    added modified changes

--- test/ut/desc/TestCaseInfoReader_UT.h
+++ test/ut/desc/TestCaseInfoReader_UT.h
@@ -9,90 +9,48 @@
 #include <testngppst/ResourceCheckPoint.h>
 
 #include <testngpp/desc/TestCaseInfoReader.h>
+#include <testngpp/desc/FixtureInfoReader.h>
+#include <testngpp/desc/ModuleFileInfoReader.h>
+#include <testngpp/desc/SuiteInfoReader.h>
+#include <testngpp/loaders/DLModuleLoader.h>
 
 USING_MOCKCPP_NS
 USING_TESTNGPP_NS
 
-
 FIXTURE(TestCaseInfoReader)
 {
-
-   struct MyTestCaseInfo
-      : public TESTNGPP_NS::TestCaseInfoReader
-   {
-      const std::string getName() const
-      { return "my test case"; }
-
-      const std::string getFixtureName() const
-      { return "my fixture"; }
-
-      const std::string getSuiteName() const
-      { return "sample"; }
-
-      const std::string getModuleFileName() const
-      { return "libMyFixture.so"; }
-
-      const std::string getFullPath() const
-      { return "/Users/darwin/testngpp/samples/MyFixture.h"; }
-
-      const std::string getFileName() const
-      { return "MyFixture.h"; }
-
-      const unsigned int getLineNumber() const
-      { return 57; }
-
-      const TestCaseInfoReader* getDependant() const
-      { return 0; }
-
-      const unsigned int numberOfTags() const
-      { return 3; }
-
-      const char* const * const getTags() const
-      {
-         static const char* const tags[] = {"ft","slow","empty"};
-         return tags;
-      }
-      
-      const unsigned int timeout() const
-      {
-         return 1200;
-      }
-   };
-
    TESTNGPPST_RCP checkpoint;
+   ModuleLoader* loader;
+   
+   typedef const TESTNGPP_NS::SuiteInfoReader * const (*SuiteGetter)();
+
+   SuiteGetter getSuite;
 
    SETUP()
    {
       checkpoint = TESTNGPPST_SET_RESOURCE_CHECK_POINT();
-      INFO("SETUP");
+      loader = new DLModuleLoader();
+      ASSERT_TRUE(0 != loader);
+
+      ASSERT_THROWS_NOTHING(loader->load("libMySuite"));
+      ASSERT_THROWS_NOTHING(getSuite = (SuiteGetter)loader->findSymbol("get_suite"));
+      ASSERT_TRUE(getSuite != 0);
    }
 
    TEARDOWN()
    {
-      INFO("TEARDOWN");
+      loader->unload();
+      delete loader;
       TESTNGPPST_VERIFY_RESOURCE_CHECK_POINT(checkpoint);
    }
 
-   TEST(Interface is defined correctly)
+   TEST("$> get suite info")
    {
-      TestCaseInfoReader* testcase = new MyTestCaseInfo();
+      const TESTNGPP_NS::SuiteInfoReader* const suite = getSuite();
 
-      std::string name = testcase->getName();
-      ASSERT_EQ("my test case", name);
-      unsigned int line = testcase->getLineNumber();
-      ASSERT_EQ(57, line);
+      ASSERT_TRUE(0 != suite);
 
-      INFO("Dependant has to be a const pointer");
-      const TestCaseInfoReader* dependant = testcase->getDependant();
-      ASSERT_TRUE(0 == dependant);
-
-      ASSERT_EQ(3, testcase->numberOfTags());
-
-      const char* const * tags = testcase->getTags();
-      ASSERT_EQ(std::string("ft"), tags[0]);
-      ASSERT_EQ(std::string("slow"), tags[1]);
-      ASSERT_EQ(std::string("empty"), tags[2]);
-
-      delete testcase;
+      ASSERT_EQ("MySuite", suite->getName());
+      ASSERT_EQ(2, suite->numberOfModuleFiles());
    }
 };
